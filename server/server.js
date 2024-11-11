9527583cb70bd09e4987d97b816b9f0cd5bfb3f9
hussain-saad99 <hussain.saad99@gmail.com>
2024-11-11 00:40:41 -0600

    Project Upload

---
+++ server/server.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const cors = require("cors");
+const OpenAI = require("openai");
+
+const app = express();
+const PORT = 3001;
+
+// Enable CORS for all routes
+app.use(cors());
+
+// SSE Endpoint
+app.get("/recipeStream", (req, res) => {
+  const ingredients = req.query.ingredients;
+  const mealType = req.query.mealType;
+  const cuisine = req.query.cuisine;
+  const cookingTime = req.query.cookingTime;
+  const complexity = req.query.complexity;
+
+  console.log(req.query)
+
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.setHeader("Connection", "keep-alive");
+
+  // Function to send messages
+  const sendEvent = (chunk) => {
+    let chunkResponse;
+    if (chunk.choices[0].finish_reason === "stop") {
+      res.write(`data: ${JSON.stringify({ action: "close" })}\n\n`);
+    } else {
+      if (
+        chunk.choices[0].delta.role &&
+        chunk.choices[0].delta.role === "assistant"
+      ) {
+        chunkResponse = {
+          action: "start",
+        };
+      } else {
+        chunkResponse = {
+          action: "chunk",
+          chunk: chunk.choices[0].delta.content,
+        };
+      }
+      res.write(`data: ${JSON.stringify(chunkResponse)}\n\n`);
+    }
+  };
+
+  const prompt = [];
+  prompt.push("You are an experienced culinary teacher with years of teaching students at all skill levels (beginner, intermediate, and expert). Generate a recipe that incorporates the following details:");
+  prompt.push(`[Ingredients: ${ingredients}]`);
+  prompt.push(`[Meal Type: ${mealType}]`);
+  prompt.push(`[Cuisine Preference: ${cuisine}]`);
+  prompt.push(`[Cooking Time: ${cookingTime}]`);
+  prompt.push(`[Complexity: ${complexity}]`);
+  ///Break up the task into multiple steps to aid in chain-of-thought prompting
+  prompt.push(
+    "Please provide a detailed recipe, including steps for preparation and cooking. Only use the ingredients provided."
+  );
+  prompt.push(
+    "The recipe should highlight the fresh and vibrant flavors of the ingredients."
+  );
+  prompt.push(
+    "Also give the recipe a suiable name in its local languagebased on cuisine preference."
+  );
+
+  const messages = [
+    {
+      role: "system",
+      content: prompt.join(" "),
+    },
+  ];
+  fetchOpenAICompletionsStream(messages, sendEvent);
+
+  // Clear interval and close connection on client disconnect
+  req.on("close", () => {
+    res.end();
+  });
+});
+
+async function fetchOpenAICompletionsStream(messages, callback) {
+  const OPENAI_API_KEY = "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";
+  console.log("OpenAI API Key:", OPENAI_API_KEY);
+  const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+  const aiModel = "gpt-3.5-turbo";
+  try {
+    const completion = await openai.chat.completions.create({
+      model: aiModel,
+      messages: messages,
+      //temperature: 1,
+      stream: true,
+    });
+
+    for await (const chunk of completion) {
+      callback(chunk);
+    }
+  } catch (error) {
+    console.error("Error fetching data from OpenAI API:", error);
+    throw new Error("Error fetching data from OpenAI API.");
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
